0af2c9cd446590a0121d6920c59839491f96a653
wesleymccollam <wesleymccollam@pingidentity.com>
2020-11-02 14:40:36 -0500

    Add newman install to script

--- .gitlab-ci/00-initialization.sh
+++ .gitlab-ci/00-initialization.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -xeo pipefail
 
-sudo npm install newman
+REQUIRED_PKG="npm"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
+npm install newman
 newman -v
 pwd
 env
