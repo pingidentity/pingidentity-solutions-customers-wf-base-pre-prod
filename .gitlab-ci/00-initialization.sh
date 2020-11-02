41c125212a5cdc56514bdf39c245edb6746d5c28
wesleymccollam <wesleymccollam@pingidentity.com>
2020-11-02 14:31:42 -0500

    Adding newman check to runner

--- .gitlab-ci/00-initialization.sh
+++ .gitlab-ci/00-initialization.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -xeo pipefail
 
+REQUIRED_PKG="newman"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
 newman -v
-
 pwd
 env
 echo "$USER"
