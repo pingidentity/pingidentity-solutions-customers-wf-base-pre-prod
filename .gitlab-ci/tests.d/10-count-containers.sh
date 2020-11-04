38e275f2c3bd8dae5318f742bce2ea7d2ffcb540
wesleymccollam <wesleymccollam@pingidentity.com>
2020-11-04 13:45:31 -0500

    Changed expected containers

--- .gitlab-ci/tests.d/10-count-containers.sh
+++ .gitlab-ci/tests.d/10-count-containers.sh
@@ -2,7 +2,7 @@
 # Verify all 6 containers run from startup
 
 CONTAINERCOUNT=$(docker ps -q $1 -f status=running | wc -l)
-CONTAINERSEXPECTED=7
+CONTAINERSEXPECTED=2
 SECONDS=0
 SECONDSMAX=3000
 SECONDSLIMIT=$(($SECONDSMAX+25))
