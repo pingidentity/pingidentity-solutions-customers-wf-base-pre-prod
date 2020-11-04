55f4da29f8389f1c3580aee0d38161debfb98e53
wesleymccollam <wesleymccollam@pingidentity.com>
2020-11-04 16:43:49 -0500

    Testing GitHub push

--- .gitlab-ci/03-publish.sh
+++ .gitlab-ci/03-publish.sh
@@ -9,17 +9,18 @@ if [[ "$GITHUBLOCATIONCHECK" == *"pingidentity-solutions-wf360.git"* ]]; then
     echo "GitHub remote location already exists!"
     GITREMOTENAME=$(echo $GITHUBLOCATIONCHECK | awk '{print $1}')
     echo "$GITREMOTENAME found... Using this to push to GitHub..."
-    git push "$GITREMOTENAME" HEAD:main
+    #git push "$GITREMOTENAME" HEAD:main
 else
     echo "Adding GitHub remote location..."
-    GITREMOTENAME="gh_location"
-    git remote add "$GITREMOTENAME" "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/pingidentity/pingidentity-solutions-wf360.git"
-    git push "$GITREMOTENAME" HEAD:main
+    echo $GITHUBLOCATION
+#    GITREMOTENAME="gh_location"
+#     git remote add "$GITREMOTENAME" "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/pingidentity/pingidentity-solutions-wf360.git"
+#     git push "$GITREMOTENAME" HEAD:main
 fi
 
-if test -n "$CI_COMMIT_TAG"
-then
-    git push "$GITREMOTENAME" "$CI_COMMIT_TAG"
-fi
+# if test -n "$CI_COMMIT_TAG"
+# then
+#     git push "$GITREMOTENAME" "$CI_COMMIT_TAG"
+#fi
 
 exit 0
