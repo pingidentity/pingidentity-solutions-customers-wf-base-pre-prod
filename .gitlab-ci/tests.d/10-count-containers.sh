f088ec84f0831cc823988a0d76fedb0876fbb2c9
jameswells <jameswells@pingidentity.com>
2020-11-05 15:42:31 -0600

    printing failing containers during count

--- .gitlab-ci/tests.d/10-count-containers.sh
+++ .gitlab-ci/tests.d/10-count-containers.sh
@@ -12,6 +12,7 @@ if [ $CONTAINERCOUNT -eq $CONTAINERSEXPECTED ]; then
 echo "$CONTAINERSEXPECTED containers expected. $CONTAINERCOUNT containers found..."
 elif [ $CONTAINERCOUNT -ne $CONTAINERSEXPECTED ]; then
 echo "$CONTAINERSEXPECTED containers expected. $CONTAINERCOUNT containers found..."
+docker ps --format '{{.Names}} {{.Status}}'
 exit 1
 fi
 
