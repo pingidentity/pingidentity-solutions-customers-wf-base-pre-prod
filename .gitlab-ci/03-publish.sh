60c12089d7e11bad2e7c59d0ef74002722b2e89d
wesleymccollam <wesleymccollam@pingidentity.com>
2020-11-10 13:18:19 -0500

    Edited Publish step

--- .gitlab-ci/03-publish.sh
+++ .gitlab-ci/03-publish.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Pushes GitLab repository to public-facing GitHub (main branch) repository
-​#set -euo pipefail
-git config --global user.email 'pd-solutions@pingidentity.com'
-git config --global user.name 'PingIdentity Solutions'
-​
+
+set -euo pipefail
+
 GITLOCATION=$(git remote -v)
 GITLOCATIONCHECK=$(echo "$GITLOCATION" | awk '/location/ && /push/')
 if [[ "$GITLOCATIONCHECK" == *"pingidentity-solutions-wf360.git"* ]]; then
@@ -16,18 +15,17 @@ else
     GITREMOTENAME=$(echo $GITLOCATIONCHECK | awk '{print $1}')
     if [[ "$GITREMOTENAME" == "gh_location" ]]; then
         echo "$GITREMOTENAME found! Removing and adding proper git remote location..."
-        git remote rm "$GITREMOTENAME"
+        git remote rm gh_location
         GITREMOTENAME="gh_location"
-        git remote set-url "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/pingidentity/pingidentity-solutions-wf360.git"
+        git remote add "$GITREMOTENAME" "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/pingidentity/pingidentity-solutions-wf360.git"
+        git push "$GITREMOTENAME" HEAD:main
+    else
+        git push "$GITREMOTENAME" HEAD:main
     fi
-    git pull "$GITREMOTENAME" HEAD:main
-    git push "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/pingidentity/pingidentity-solutions-wf360.git" HEAD:main
 fi
-​
+
 if test -n "$CI_COMMIT_TAG"
 then
     echo "using $GITREMOTENAME..."
-    git push "$GITREMOTENAME" --tags "$CI_COMMIT_TAG"
+    git push "$GITREMOTENAME" "$CI_COMMIT_TAG"
 fi
-​
-exit 0
