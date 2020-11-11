ac52da10b15b515a031a9c2186fc0e3cbd337d74
jameswells <jameswells@pingidentity.com>
2020-11-11 09:16:59 -0600

    testing runner variables

--- .gitlab-ci/tests.d/20-postman-test.sh
+++ .gitlab-ci/tests.d/20-postman-test.sh
@@ -4,7 +4,7 @@
 #get docker network name
 DOCKER_NW=$(docker network ls | grep workforce | awk '{print $2}')
 
-POSTMANRUN=$(docker run -t --network="$DOCKER_NW" postman/newman run --insecure https://api.getpostman.com/collections/12861248-cc188562-59d9-4326-9fc0-6bcbd709f934?apikey=PMAK-5fa05c0fc71f0b0034f573fa-bdbe88e369e218ea4fc92d2c86bec6b555)
+POSTMANRUN=$(docker run -t --network="$DOCKER_NW" postman/newman run --insecure https://api.getpostman.com/collections/12861248-cc188562-59d9-4326-9fc0-6bcbd709f934?apikey=POSTMAN_API)
 
 # Filters results and returns as a 6 digit number - expected to have 00000 for successful testing
 RESULT=$(echo "$POSTMANRUN" | grep -A 8 "iterations" | awk '{print $6}' | sed -e '/^$/d' | tr -d '\n')
