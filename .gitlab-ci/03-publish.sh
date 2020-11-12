256dae7a081de96a4cef2e1b0d4754f231bed5e3
Wesley McCollam <wesleymccollam@pingidentity.com>
2020-11-12 16:58:55 +0000

    Update 03-publish.sh

--- .gitlab-ci/03-publish.sh
+++ .gitlab-ci/03-publish.sh
@@ -10,7 +10,7 @@ if [[ "$GITLOCATIONCHECK" == *"pingidentity-solutions-wf360.git"* ]]; then
     GITREMOTENAME=$(echo $GITLOCATIONCHECK | awk '{print $1}')
     echo "$GITREMOTENAME found... Using this to push to GitHub..."
     git fetch --unshallow
-    git push "$GITREMOTENAME" HEAD:main
+    git push -f "$GITREMOTENAME" HEAD:main
 else
     echo "Adding Git remote location..."
     GITREMOTENAME=$(echo $GITLOCATIONCHECK | awk '{print $1}')
@@ -21,13 +21,13 @@ else
         echo "$GITREMOTENAME"
         git remote add $GITREMOTENAME "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/pingidentity/pingidentity-solutions-wf360.git"
         git fetch --unshallow
-        git push $GITREMOTENAME HEAD:main
+        git push -f $GITREMOTENAME HEAD:main
     else
         GITREMOTENAME="gh_location"
         echo "$GITREMOTENAME found! Removing and adding proper git remote location..."
         git remote add $GITREMOTENAME "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/pingidentity/pingidentity-solutions-wf360.git"
         git fetch --unshallow
-        git push $GITREMOTENAME HEAD:main
+        git push -f $GITREMOTENAME HEAD:main
     fi
 fi
 
