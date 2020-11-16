09decd3e613fe58d27d769579349707ac16f67c9
Wesley McCollam <wesleymccollam@pingidentity.com>
2020-11-16 17:30:07 +0000

    Updated 03-publish.sh to reflect new GitHub repo naming

--- .gitlab-ci/03-publish.sh
+++ .gitlab-ci/03-publish.sh
@@ -5,7 +5,7 @@ set -x
 
 GITLOCATION=$(git remote -v)
 GITLOCATIONCHECK=$(echo "$GITLOCATION" | awk '/fetch/ && /push/')
-if [[ "$GITLOCATIONCHECK" == *"pingidentity-solutions-wf360.git"* ]]; then
+if [[ "$GITLOCATIONCHECK" == *"pingidentity-solutions-wf360.git"* ]] || [[ "$GITLOCATIONCHECK" == *"wf-base-pre-prod.git"* ]]; then
     echo "Git remote location already exists!"
     GITREMOTENAME=$(echo $GITLOCATIONCHECK | awk '{print $1}')
     echo "$GITREMOTENAME found... Using this to push to GitHub..."
@@ -19,13 +19,13 @@ else
         git remote rm gh_location
         GITREMOTENAME="gh_location"
         echo "$GITREMOTENAME"
-        git remote add $GITREMOTENAME "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/pingidentity/pingidentity-solutions-wf360.git"
+        git remote add $GITREMOTENAME "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/pingidentity/pingidentity-solutions-customers-wf-base-pre-prod.git"
         git fetch --unshallow
         git push $GITREMOTENAME HEAD:main
     else
         GITREMOTENAME="gh_location"
         echo "$GITREMOTENAME found! Removing and adding proper git remote location..."
-        git remote add $GITREMOTENAME "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/pingidentity/pingidentity-solutions-wf360.git"
+        git remote add $GITREMOTENAME "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/pingidentity/pingidentity-solutions-customers-wf-base-pre-prod.git"
         git fetch --unshallow
         git push $GITREMOTENAME HEAD:main
     fi
