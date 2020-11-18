fdabcb27939839d8c812974038bcb9684ed1759d
jameswells <jameswells@pingidentity.com>
2020-11-18 13:46:57 -0600

    updated method to count containers and compare

--- .gitlab-ci/tests.d/10-count-containers.sh
+++ .gitlab-ci/tests.d/10-count-containers.sh
@@ -2,21 +2,22 @@
 # Verify all 6 containers run from startup
 #set -x
 
-CONTAINERCOUNT=$(docker ps --format '{{.Names}}' -f status=running | grep -v pingcentral-db-loader | wc -l)
-CONTAINERSEXPECTED=7
+#ignoring dbloader because we don't care about that. get the list of container names from docker-compose and sort
+EXPECTED_CONT=$(cat docker-compose.yaml | grep -B 1 "image:" | sed -e '/image/d' -e '/loader/d' -e '/--/d' -e s'/://' -e 's/^[[:space:]]*//'  | sort -u)
+CONTAINERSEXPECTED=$(echo "$EXPECTED_CONT" | wc -l)
 SECONDS=0
-SECONDSMAX=3000
-SECONDSLIMIT=$(($SECONDSMAX+25))
+SECONDSLIMIT=3000
 
 while [ $SECONDS -le $SECONDSLIMIT ]
 do
-    CONT_STATUS=$(docker ps --format '{{.Names}} {{.Status}}')
+    CONT_STATUS=$(docker ps --format '{{.Names}} {{.Status}}' | sort -u)
     #check that we still have $CONTAINERSEXPECTED num of containers. error if we don't.
     CONTAINERCOUNT=$(echo "$CONT_STATUS" | grep -v pingcentral-db-loader | wc -l)
-    if [ $CONTAINERCOUNT -ne $CONTAINERSEXPECTED ]; then
+    if [ $CONTAINERCOUNT -lt $CONTAINERSEXPECTED ]; then
         #if there's less containers (i.e. directory dies), print the docker logs and exit 1.
         echo "$CONTAINERSEXPECTED containers expected. $CONTAINERCOUNT containers found..."
-    echo "$CONT_STATUS"
+        echo "$CONT_STATUS"
+        diff <(echo "$CONT_STATUS" | sed -e s'@'"${PWD##*/}_"'@@' -e s'/_.*//') <(echo "$EXPECTED_CONT") | awk '{print "Warning: Container(s) " $0 " stopped"}'
         docker-compose logs --tail="100"
         exit 1
     fi
@@ -51,12 +52,11 @@ do
     exit 1
     fi
 
-    #if no containers remaining in capture move on
-    if [ "${STARTING_CONT:-0}" == 0 ]; then
+    #if no containers remaining in capture move on (all containers need to be running)
+    if [ "${STARTING_CONT:-0}" == 0 ] && [ $CONTAINERCOUNT -eq $CONTAINERSEXPECTED ]; then
     exit 0
     fi 
 
     #wait
     sleep 30
 done
-
